6c5c03ae54f1f6ad2d7410b39f35d9e6603ba6df
Andrés <203389650+23ananby@users.noreply.github.com>
2026-08-18 00:37:00 -0600

    chore(ci): update deployment path and workflow
    Configure base path to /datav-s4/ and integrate GitHub Pages setup action for deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: '/datav-s4/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
